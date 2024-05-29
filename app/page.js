a6015b5efc46f372b0f745e0c3ce2ff90859255f
VijayDeshwal339 <deshwalamit339@gmail.com>
2024-05-29 18:39:49 +0530

    done

--- app/page.js
+++ app/page.js
@@ -51,7 +51,7 @@ export default function Home() {
       </div>
 
       {selectedProduct && (
-        <div className="md:px-60 fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <div className=" px-4 md:px-60 fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg relative">
             <button className="absolute top-2 right-2 text-xl" onClick={handleCloseModal}>&times;</button>
             <img src={selectedProduct.image} alt={selectedProduct.title} className=" w-full h-64 object-scale-down mb-4 rounded" />
